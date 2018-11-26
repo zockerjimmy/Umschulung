8347fcb0af8a25a59e38df54bd8be805120c076d
Alexander Schmidt <main.schmidt.alexander@gmail.com>
2018-11-26 18:13:33 +0100

    Zahlenraten

--- Zahlenraten/src/Main.java
+++ Zahlenraten/src/Main.java
@@ -3,27 +3,59 @@ import java.util.Scanner;
 
 public class Main
 {
-
-	//Schauen Sie in Tag2 Aufgaben und ergänzen Sie die Hardgecodeten Eingaben mit dynamischen
-	
-	//Nehmen Sie eine Zahl an, Konsole leeren, Neue Zahl annehmen, überprüfen ob größer oder kleiner, 
-	//Hinweis geben, Wenn Abstand kleiner als 5 = besonderer Hinweis, Begrenze auf 5 Raterunden
-	//wenn Zahl gefunden, gebe Zahl gefunden aus, ansonsten "leider verloren"
+	// Nehmen Sie eine Zahl an, Konsole leeren, Neue Zahl annehmen, überprüfen ob
+	// größer oder kleiner,
+	// Hinweis geben, Wenn Abstand kleiner als 5 = besonderer Hinweis, Begrenze auf
+	// 5 Raterunden
+	// wenn Zahl gefunden, gebe Zahl gefunden aus, ansonsten "leider verloren"
 	public static void main(String[] args)
 	{
 		Scanner scanner = new Scanner(System.in);
 		int goalValue;
 		int userInput;
-		
+		int counter = 0;
+		int counterMax = 5;
+
 		out.print("Zielwert: ");
 		goalValue = scanner.nextInt();
-		
-		for(int i = 0; i < 100; i++)
+
+		for (int i = 0; i < 10; i++)
 		{
 			out.println();
 		}
-		
-		
+
+		do
+		{
+			out.println();
+			out.print("Eingabe: ");
+			userInput = scanner.nextInt();
+			if (userInput == goalValue)
+			{
+				out.println("Korrekt!");
+				break;
+			} else if (userInput > goalValue)
+			{
+				out.println("Zu hoch!");
+			} else if (userInput < goalValue)
+			{
+				out.println("Zu niedrig!");
+			}
+
+			if ((goalValue - userInput) < 5 && (goalValue - userInput) > -5 && (goalValue - userInput) != 0)
+			{
+				out.println("Knapp!");
+			}
+			counter++;
+			if (counter == counterMax)
+			{
+				out.print("Vorbei!");
+			}
+		} while (counter < counterMax);
+
+		scanner.close();
+
+		// int x = 4;
+		// System.out.print((x&1) == 1 ?(x + " is ungerade") : (x + " is gerade"));
 	}
 
 }
