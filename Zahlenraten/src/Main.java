5f57d8e2d7b6102e8a6c35b18bc9a44e2e101fe8
Alexander Schmidt <main.schmidt.alexander@gmail.com>
2018-11-26 12:53:24 +0100

    Update

--- Zahlenraten/src/Main.java
+++ Zahlenraten/src/Main.java
@@ -1,14 +1,29 @@
 import static java.lang.System.out;
+import java.util.Scanner;
 
 public class Main
 {
 
 	//Schauen Sie in Tag2 Aufgaben und ergänzen Sie die Hardgecodeten Eingaben mit dynamischen
-	//Nehmen Sie eine Zahl an, Konsole leeren, Neue Zahl annehmen, überprüfen ob größer oder kleiner, Hinweis geben, Wenn Abstand kleiner als 5 = besonderer Hinweis, Begrenze auf 5 Raterunden
+	
+	//Nehmen Sie eine Zahl an, Konsole leeren, Neue Zahl annehmen, überprüfen ob größer oder kleiner, 
+	//Hinweis geben, Wenn Abstand kleiner als 5 = besonderer Hinweis, Begrenze auf 5 Raterunden
+	//wenn Zahl gefunden, gebe Zahl gefunden aus, ansonsten "leider verloren"
 	public static void main(String[] args)
 	{
-
-
+		Scanner scanner = new Scanner(System.in);
+		int goalValue;
+		int userInput;
+		
+		out.print("Zielwert: ");
+		goalValue = scanner.nextInt();
+		
+		for(int i = 0; i < 100; i++)
+		{
+			out.println();
+		}
+		
+		
 	}
 
 }
